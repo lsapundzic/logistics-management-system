97ee7904a56e279377308fdaaf6408a906f409cb
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2024-08-05 09:15:22 +0200

    navigation functional

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import Header from "./components/header"
 
 
 export default function Home() {
   return (
     <div>
+      <Header />
       <h1>Hello</h1>
     </div>
   );
---
+++ app/settings/page.tsx
@@ -0,0 +1,10 @@
+import Header from "../components/header"
+
+export default function Settings() {
+    return (
+        <div>
+            <Header />
+            <h1>Welcome to Settings</h1>
+        </div>
+    )
+}
