01b8e82a83dee653e3149f92df402660f6ec9d83
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2024-08-07 23:50:32 +0200

    tested object functionality

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -1,10 +1,16 @@
 import Header from "../components/header"
 
 export default function List() {
+
+    const myList = ["Jackie", "Johnnie"]
     return (
         <div>
             <Header />
             <h1>Welcome to Lists</h1>
+            <h2>List</h2>
+            <p>
+                {myList[1]}
+            </p>
         </div>
     )
 }
